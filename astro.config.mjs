46c724e12a102733f53542f995fcc2d69971590c
Andrijan Tasevski <andrijantasevski@gmail.com>
2024-02-14 19:00:35 +0100

    feat: added open graph and prettier plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://early20s.andrijan.dev",
-  integrations: [tailwind(), mdx(), sitemap()]
+  integrations: [tailwind(), mdx(), sitemap()],
 });
