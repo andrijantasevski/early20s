2ab56b259f7b0e0915839a8c1dd16b4277ce8a64
Andrijan Tasevski <andrijantasevski@gmail.com>
2024-02-14 18:33:39 +0100

    feat: added sitemap integration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: "https://early20s.andrijan.dev",
+  integrations: [tailwind(), mdx(), sitemap()]
 });
